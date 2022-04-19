069ec1927460ee49dfbe1fbab9de409fffdd9100
Arjun Saili <arjun7.saili@gmail.com>
2022-04-19 01:15:43 -0400

    Create routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <SignUp/> 
-      </div>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/signup" exact element={<SignUp />}/>
+          <Route path="/login" exact element={<Login />}/>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
