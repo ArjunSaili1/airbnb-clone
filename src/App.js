7859b01272b712aa0e4887a5d09c92bb75b2291c
Arjun Saili <arjun7.saili@gmail.com>
2022-04-28 02:41:26 -0400

    require auth for forgotpass, remove private route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import UpdateAccount from './components/UpdateProfile';
-import RequireBooking from './components/RequireBooking';
 import RequireAuth from './components/RequireAuth';
 import Login from './components/Login';
 import ForgotPassword from './components/ForgotPassword';
@@ -31,19 +30,22 @@ function App() {
                 <Login />
               </RequireNotUser>
             }/>
-            <Route path="/forgot-password" element={<ForgotPassword/>}/>
+            <Route path="/forgot-password" 
+            element={
+              <RequireNotUser>
+                <ForgotPassword/>
+              </RequireNotUser>
+            }/>
             <Route path="/bookings"
             element={
               <RequireAuth>
-                <RequireBooking>
-                  <Bookings/>
-                </RequireBooking>
+                <Bookings/>
               </RequireAuth>
             }/>
             <Route path="/book-home" element={
-            <RequireAuth>
-              <BookHome/>
-            </RequireAuth>}/>
+              <RequireAuth>
+                <BookHome/>
+              </RequireAuth>}/>
             <Route path="/update-profile" element={
               <RequireAuth>
                 <UpdateAccount/>
