eb29d22780596f6a7c0de2b48c05e9c630d56037
Arjun Saili <arjun7.saili@gmail.com>
2022-04-27 23:39:39 -0400

    add framer motion, create booking carousel & style
    update sizing and position of elements after header, style booking item

--- src/components/BookingCarousel.js
+++ src/components/BookingCarousel.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useDb } from '../contexts/DatabaseContext'
+import { motion } from 'framer-motion'
 import BookingOption from './BookingOption'
 
 export default function BookingCarousel() {
 
     const { getBookingOptions } = useDb()
     const [options, setOptions] = useState(null)
+    const [position, setPosition] = useState(0);
 
     useEffect(()=>{
         async function setBookingOptions(){
@@ -15,12 +17,48 @@ export default function BookingCarousel() {
         setBookingOptions()
     },[getBookingOptions])
 
+    function moveRight(){
+        if(position === options.length - 1){
+            setPosition(0)
+        }
+        if(position < options.length - 2){
+            setPosition(position + 1)
+        }
+
+    }
+
+    function moveLeft(){
+        if(position === options.length - 1){
+            setPosition(0)
+        }
+        if(position > -1){
+            setPosition(position - 1)
+        } 
+    }
+
     return (
-    <div>
-        {options ? options.map((location)=>{
-            const {address, city, description, name, id} = location;
-            return <BookingOption key={id} id={id} address={address} city={city} description={description} name={name}/>
-        }): null}
-    </div>
+    <>
+        <div className="booking-carousel-ctn">
+            <div className="booking-carousel">
+                {options ? options.map((location, index )=>{
+                    const {address, city, description, name, id} = location;
+                    return(
+                    <BookingOption key={id} 
+                    id={id} 
+                    address={address}  
+                    city={city} 
+                    description={description} 
+                    name={name}
+                    index={index}
+                    position={position}/>
+                    )
+                }): null}
+            </div>
+        </div>
+        <div className="carousel-btns" style={{width: "100vw"}}>
+            <button onClick={moveRight} style={{right: 0}}>Right</button>
+            <button onClick={moveLeft} style={{left: 0}}>Left</button>
+        </div>
+    </>
     )
 }
--- src/components/BookingOption.js
+++ src/components/BookingOption.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { ref, getDownloadURL } from "firebase/storage";
 import { storage } from '../firebase';
+import { motion } from 'framer-motion';  
 
-export default function BookingOption({address, id, city, description, name}) {
+export default function BookingOption({address, id, city, description, name, index, position}) {
 
     const [image, setImage] = useState(null);
 
@@ -11,17 +12,31 @@ export default function BookingOption({address, id, city, description, name}) {
             const imgSrc = await getDownloadURL(ref(storage, `images/${id}.jpg`))
             setImage(imgSrc);
         }
-
         getBookingImage();
     },[id])
 
     return (
-    <div>
-        <h1>{address}</h1>
-        <h1>{city}</h1>
-        <h1>{description}</h1>
+    <motion.article className="booking-option"
+    style={index === position + 1? {zIndex: 100}: null}
+    initial={{
+        rotation: -180, 
+        scale: 0}}
+    animate={{
+        rotation: 0, 
+        scale: index === position + 1? 1 : 0.8,
+        left: `${(index - position) * 35 - 52.5}vw `
+    }}
+    transition={{
+        damping: 20
+    }}>
         <img className="booking-img" src={image} alt={name}/>
-        <h1>{name}</h1>
-    </div>
+        <div className="booking-details">
+            <h3>{name}</h3>
+            <h4>{address}</h4>
+            <h4>{city}</h4>
+            <h5>{description}</h5>
+        </div>
+        <button className="book-btn">Book</button>
+    </motion.article>
     )
 }
