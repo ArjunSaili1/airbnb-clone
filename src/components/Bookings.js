cd2ed4e768021f7ae28d217017493548c535aced
Arjun Saili <arjun7.saili@gmail.com>
2022-04-28 02:38:25 -0400

    Replace private route component w/ reroute on page

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useNavigate } from 'react-router';
+import { useDb } from '../contexts/DatabaseContext';
 import Header from './Header'
 
 export default function Bookings() {
-  return (
-    <div className="page">
-      <Header/>
-      <h3 className="page-content">Bookings</h3>
-    </div>
+  const [loading, setLoading] = useState(true);
+  const [hasBooked, setHasBooked] = useState("init")
+  const {bookingExists} = useDb();
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+      async function checkIfBooked(){
+          const {booking} = await bookingExists();
+          if(booking){setHasBooked(true)}
+          else{setHasBooked(false)}
+          setLoading(false)
+      }
+      checkIfBooked();
+  }, [bookingExists])
+
+  return(<>
+    {
+    loading ? null :
+    hasBooked ?
+      <div className="page">
+        <Header/>
+        <div className="page-content">
+          <h1>Bookings</h1>
+        </div>
+      </div>
+    : navigate("/book-home")}
+    </>
   )
 }
