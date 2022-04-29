02db7e943e80afa1094d907cda96de9d360baa92
Arjun Saili <arjun7.saili@gmail.com>
2022-04-29 02:23:35 -0400

    Remove check if already booked for routing

--- src/components/MyBooking.js
+++ src/components/MyBooking.js
@@ -1,35 +1,13 @@
-import React, {useEffect, useState} from 'react'
-import { useNavigate } from 'react-router';
-import { useDb } from '../contexts/DatabaseContext';
+import React from 'react'
 import Header from './Header'
 
 export default function MyBooking() {
-  const [loading, setLoading] = useState(true);
-  const [hasBooked, setHasBooked] = useState("init")
-  const {bookingExists} = useDb();
-  const navigate = useNavigate()
-
-  useEffect(()=>{
-      async function checkIfBooked(){
-          const {booking} = await bookingExists();
-          if(booking){setHasBooked(true)}
-          else{setHasBooked(false)}
-          setLoading(false)
-      }
-      checkIfBooked();
-  }, [bookingExists])
-
-  return(<>
-    {
-    loading ? null :
-    hasBooked ?
-      <div className="page">
-        <Header/>
-        <div className="page-content">
-          <h1>MyBooking</h1>
-        </div>
-      </div>
-    : navigate("/book-home")}
-    </>
+  return(
+  <div className="page">
+    <Header/>
+    <div className="page-content">
+      <h1>My Booking</h1>
+    </div>
+  </div>
   )
 }
