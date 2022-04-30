ffecff8e0d5f6972f8e8598cd490f73abc749da6
Arjun Saili <arjun7.saili@gmail.com>
2022-04-29 23:58:03 -0400

    hold data in local state until all fields set
    set local state in page comp, check for set booking & show warning

--- src/components/BookHome.js
+++ src/components/BookHome.js
@@ -6,15 +6,35 @@ import SetDatesModal from './SetDatesModal'
 import BookingWarning from './BookingWarning'
 import BookingCarousel from './BookingCarousel'
 import { useDb } from '../contexts/DatabaseContext'
+import { useNavigate } from 'react-router'
 
 export default function BookHome() {
 
+  const navigate = useNavigate()
   const [modalScreen, setModalScreen] = useState("date")
-  const {hasBooking} = useDb();
+  const [location, setLocation] = useState(null);
+  const [checkIn, setCheckIn] = useState(null);
+  const [checkOut, setCheckOut] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const {bookingData, addData} = useDb();
 
   useEffect(()=>{
-    if(hasBooking){setModalScreen("warning")}
-  }, [hasBooking])
+    if(bookingData["checkIn"] && bookingData["checkOut"] && bookingData["locationId"]){setModalScreen("warning")}
+  }, [bookingData])
+
+  useEffect(()=>{
+    async function addBooking(){
+      setLoading(true)
+      await addData("checkIn", checkIn);
+      await addData("checkOut", checkOut);
+      await addData("locationId", location);
+      setLoading(false)
+      navigate("/my-booking")
+    }
+    if(checkIn && checkOut && location){
+      addBooking()
+    }
+  }, [checkIn, checkOut, location, addData, navigate, bookingData])
 
   function removeWarning(){
     setModalScreen("date")
@@ -25,11 +45,11 @@ export default function BookHome() {
       <Header/>
       <main className="page-content">
         {modalScreen === "loading" ? null :
-        !modalScreen ? <BookingCarousel/> :
+        !modalScreen ? <BookingCarousel loading={loading} setLocation={setLocation}/> :
         <div className="modal-container">
           <div className="modal">
             {modalScreen === "date" ?
-            <SetDatesModal setModalScreen={setModalScreen}/> :
+            <SetDatesModal setCheckIn={setCheckIn} setCheckOut={setCheckOut} setModalScreen={setModalScreen}/> :
             modalScreen === "location" ? 
             <SetLocationModal setModalScreen={setModalScreen}/> :
             modalScreen === "warning" ?
