74d26393ff3b8c982eb4a0125d2001bdc4cb1e4c
Arjun Saili <arjun7.saili@gmail.com>
2022-05-02 20:11:53 -0400

    create state for newbooking
    fixes error where booking exists warning would flash before page sweitch

--- src/components/BookHome.js
+++ src/components/BookHome.js
@@ -15,21 +15,24 @@ export default function BookHome() {
   const [location, setLocation] = useState(null);
   const [checkIn, setCheckIn] = useState(null);
   const [checkOut, setCheckOut] = useState(null);
+  const [newBooking, setNewBooking] = useState(false)
   const [loading, setLoading] = useState(false);
   const {bookingData, addData} = useDb();
 
   useEffect(()=>{
-    if(bookingData["checkIn"] && bookingData["checkOut"] && bookingData["locationId"]){setModalScreen("warning")}
-  }, [bookingData])
+    if(bookingData["checkIn"] && bookingData["checkOut"] && bookingData["locationId"] &&!newBooking){setModalScreen("warning")}
+  }, [bookingData, newBooking])
 
   useEffect(()=>{
     async function addBooking(){
+      setNewBooking(true)
       setLoading(true)
       await addData("checkIn", checkIn);
       await addData("checkOut", checkOut);
       await addData("locationId", location);
       setLoading(false)
       navigate("/my-booking")
+      return;
     }
     if(checkIn && checkOut && location){
       addBooking()
