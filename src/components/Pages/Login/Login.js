ea17e8af7dc9bd1f5118bdd1b432a3bb4dd8f34d
Arjun Saili <arjun7.saili@gmail.com>
2022-05-04 11:06:27 -0400

    Create guest login

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -13,6 +13,12 @@ function Login() {
   const [loading, setLoading] = useState(false);
   const { login } = useAuth();
 
+  function guestLogin(e){
+    emailRef.current.value = "test@test.com";
+    passwordRef.current.value = "password";
+    handleLogin(e);
+  }
+
   async function handleLogin(e){
     e.preventDefault();
     setError('');
@@ -30,25 +36,33 @@ function Login() {
 
   return (
     <ModalWrapper>
-        <Modal>
+        <Modal as="form" onSubmit={handleLogin}>
           <ModalHeader>
             <h1>Login</h1>
             <h5 style={{margin: "10px 0"}}>Don't have an account? <Link to="/signup">Sign up</Link></h5>
-            {error ? <h5>{error}</h5> : null}
+            {error ? <h5 style={{color: "red"}}>{error}</h5> : null}
           </ModalHeader>
           <AuthFormField>
             <label htmlFor="email">Email</label>
-            <input ref={emailRef} autoComplete="on" type="email"></input>
+            <input tabIndex="1" ref={emailRef} autoComplete="on" type="email"></input>
+            <div style={{fontSize: "0.7em"}}>
+              Just Visiting? <span onClick={guestLogin} 
+              style={{
+                cursor: "pointer",
+                fontSize: "inherit", 
+                fontWeight: "bold"}}>Login as a guest</span>
+            </div>
           </AuthFormField>
           <AuthFormField>
             <label htmlFor="password">Password</label>
-            <input ref={passwordRef} autoComplete="on" type="password"></input>
-            <Link style={{width: "fit-content"}} to="/forgot-password"><h6>Forget your password?</h6></Link>
+            <input tabIndex="2"   ref={passwordRef} autoComplete="on" type="password"></input>
+            <Link style={{width: "fit-content", fontSize: "0.7em", fontWeight: "bold"}} to="/forgot-password">Forget your password?</Link>
           </AuthFormField>
           <div>
-            <Button 
-            disabled={loading} 
-            onClick={handleLogin}>Login</Button>
+            <Button
+            type="submit"
+            tabIndex="3   "
+            disabled={loading}>Login</Button>
           </div>
       </Modal>
     </ModalWrapper>
