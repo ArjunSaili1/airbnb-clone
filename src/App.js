5c1ceadedf658cca09661012efd9e3cc34d77159
Arjun Saili <arjun7.saili@gmail.com>
2022-05-04 03:50:45 -0400

    Remove unneeded CSS File

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import UpdateAccount from './components/UpdateProfile';
