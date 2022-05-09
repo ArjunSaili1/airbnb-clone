76a3fddf0cb7f9fa65e357a8e72b40af265e5c6d
Arjun Saili <arjun7.saili@gmail.com>
2022-05-08 22:03:34 -0400

    Remove overflowing margin

--- src/components/Pages/BookAHome/BookingCarousel/BookingCarousel.styled.js
+++ src/components/Pages/BookAHome/BookingCarousel/BookingCarousel.styled.js
@@ -20,7 +20,6 @@ const CarouselButtons = styled.div`
     width: 100%;
     display: flex;
     position: relative;
-    margin-left: 2%;
     height: 100%;
     justify-content: space-between;
     align-items: center;
